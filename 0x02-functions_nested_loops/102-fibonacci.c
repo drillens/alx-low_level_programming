30a83e51985bdcff7e69ec787d53987042daa809
Jerry Fidel <jerryfidel6@gmail.com>
2022-07-05 15:44:41 -0700

    final

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,25 +6,20 @@
  */
 int main(void)
 {
-	long int i, j, k, next;
+	long int n1 = 1, result, n2 = 2, nLast = 50;
+	int ii;
 
-	j = 1;
-
-	k = 2;
-
-	for (i = 1; i <= 50; ++i)
+	printf("%lu, %lu, ", n1, n2);
+	for (ii = 2; ii < nLast; ii++)
 	{
-		if (j != 20365011074)
-		{
-			printf("%ld, ", j);
-		}
-	       	else
-		{
-			printf("%ld\n", j);
-			next = j + k;
-			j = k;
-			k = next;
-		}
+		result = n1 + n2;
+		n1 = n2;
+		n2 = result;
+		printf("%lu", result);
+		if (ii != nLast - 1)
+			printf(", ");
+	}
+	printf("\n");
+	return (0);
 
-		return (0);
 }
