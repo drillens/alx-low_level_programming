28a4100c1fb99b0b6ab15809358ae0c264d0b37e
Jerry Fidel <jerryfidel6@gmail.com>
2022-07-07 11:56:07 -0700

    updates

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+ 16 lines (15 sloc) 224 Bytes
+#include "main.h"
+/**
+ * print_numbers - prints the numbers, from 0 to 9, followed by a new line
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	char n = 0;
+
+	while (n <= 9)
+	{
+		_putchar('0' + n);
+		n++;
+	}
+	_putchar('\n');
+}
